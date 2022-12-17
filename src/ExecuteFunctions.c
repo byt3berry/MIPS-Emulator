f6bcb65cdd510cec041e3a1dcc04032124457cdf
0xmathis <cocagnemathis@gmail.com>
2022-12-17 18:37:05 +0100

    Presque fini

---
+++ src/ExecuteFunctions.c
@@ -0,0 +1,20 @@
+//
+// Created by mathis on 12/17/22.
+//
+//
+//#include "ExecuteFunctions.h"
+//#include "constantes.h"
+//
+//
+//void initExecuteFunctions() {
+//    executeFunctions[JUMP] = jump;
+//    executeFunctions[BRANCH] = branch;
+//    executeFunctions[SHIFT_LEFT] = shiftLeft;
+//    executeFunctions[ADD] = add;
+//    executeFunctions[LOGICAL] = logical;
+//    executeFunctions[MOVE_FROM] = moveFrom;
+//    executeFunctions[ROTATE] = rotate;
+//    executeFunctions[DIVIDE] = divide;
+//    executeFunctions[MULTIPLY] = multiply;
+//    executeFunctions[MEMORY] = memory;
+//}
